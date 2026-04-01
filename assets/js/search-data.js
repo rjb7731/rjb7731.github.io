ac05ca362a71c2941c8861c33383ee75be644df4
rjb7731 <49943565+rjb7731@users.noreply.github.com>
2026-04-01 19:59:05 +0000

    Deploying to gh-pages from @ rjb7731/rjb7731.github.io@ba798be3bd3e32d711b16d99b278be96018e0d24 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,28 +16,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-building-a-gaia-1-style-world-model-for-indoor-robot-navigation",
+        },{id: "post-building-a-gaia-1-style-world-model-for-indoor-robot-navigation",
         
           title: "Building a GAIA-1 Style World Model for Indoor Robot Navigation",
         
